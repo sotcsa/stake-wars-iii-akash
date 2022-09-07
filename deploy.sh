6eed563eeeca0ea5becb9e963ae69a20d975fa11
Csaba Soti - Ext <csaba.soti@dotmatics.com>
2022-09-07 09:34:20 +0200

    fixes

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ export NEAR_ENV=testnet
 echo 'export NEAR_ENV=testnet' >> ~/.bashrc
 near proposals
 
-echo  ===================near установлен ===================
+echo  ===================near  ===================
 
 sleep 10
 sudo apt install -y git binutils-dev libcurl4-openssl-dev zlib1g-dev libdw-dev libiberty-dev cmake gcc g++ python docker.io protobuf-compiler libssl-dev pkg-config clang llvm cargo
@@ -66,30 +66,36 @@ wget -O /root/.near/config.json "https://s3-us-west-1.amazonaws.com/build.nearpr
 sleep 5
 sudo apt-get install awscli -y
 pwd
-sleep 10
-cd /root/.near/
-rm /root/.near/genesis.json
-wget https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/testnet/genesis.json
-sleep 10
+#sleep 10
+#cd /root/.near/
+#rm /root/.near/genesis.json
+#wget -c https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/testnet/genesis.json
+#sleep 10
 cd /root/.near/
 pip3 install awscli --upgrade
 sleep 20
 if  [[  -z $link_key  ]]
 then
-#tail -200 /var/log/$binary/current
-echo ====================================================================================================
-echo ====== validator_key.json not found, please create and completed of registration your account ======
-echo ====================================================================================================
-echo ===================================================================================================================================
-echo ===== Refer to instructions to address https://github.com/Dimokus88/near/blob/main/Guide_EN.md#create-and-register-a-validator ====
-echo ===================================================================================================================================
-sleep infinity
+	#tail -200 /var/log/$binary/current
+	echo ====================================================================================================
+	echo ====== validator_key.json not found, please create and completed of registration your account ======
+	echo ====================================================================================================
+	echo ===================================================================================================================================
+	echo ===== Refer to instructions to address https://github.com/Dimokus88/near/blob/main/Guide_EN.md#create-and-register-a-validator ====
+	echo ===================================================================================================================================
+	sleep infinity
 fi
+
 echo ===============================================================
 echo ====== validator_key.json is found, start validator node ======
 echo ===============================================================
 rm /root/.near/validator_key.json
 wget -O /root/.near/validator_key.json $link_key 
+
+#=========== Get data backup============
+aws s3 --no-sign-request cp s3://near-protocol-public/backups/testnet/rpc/latest .
+LATEST=$(cat latest)
+aws s3 --no-sign-request cp --no-sign-request --recursive s3://near-protocol-public/backups/testnet/rpc/$LATEST /root/.near/data
 
 #===========RUN NODE============
 echo =Run node...=
